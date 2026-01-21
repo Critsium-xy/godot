0d2da41b65e9ff58b795287d80ae97b2f8671134
Critsium-xy <tsfxwbbzxy@163.com>
2026-01-21 19:05:33 +0800

    Update compiling script

--- win_compile.bat
+++ win_compile.bat
@@ -4,4 +4,8 @@ git pull
 git submodule update --init --recursive
 echo Start Compiling...
 scons platform=windows
+echo Copying Steam Libraries ...
+copy modules\godotsteam\sdk\redistributable_bin\win64\steam_api64.dll bin\
+copy modules\godotsteam\sdk\redistributable_bin\win64\steam_api64.lib bin\
+echo Build Complete! Find your build in the 'bin' folder.
 pause
